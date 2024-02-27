98d61182007d9f44db38cddc61d4bd7db1939eb4
Zainab Al-Najjar <eng.zainab.alnajar@gmail.com>
2024-02-27 16:40:06 +0300

    Completed assignment!

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name,email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+        }),
+      })
+      .then(response => response.json())
+      .then(data => {
+        const id = document.createElement("p");
+        id.innerHTML= `User ID: ${data.id}`;  
+        const body = document.querySelector('body'); 
+        body.appendChild(id)
+        })
+        .catch(error => {
+            const err = document.createElement("p");
+            err.innerHTML= `Error: ${error.message}`;  
+            const body = document.querySelector('body'); 
+            body.appendChild(err)
+            console.log(error.message)
+        }); 
+
+
+}
